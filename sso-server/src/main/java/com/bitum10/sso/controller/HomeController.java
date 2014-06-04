c5cc7bee5b7860aca105ef7a81899ec6c4ce4de6
cncduLee <shouli1990@gmail.com>
2014-06-04 10:46:07 +0800

    adjust the freamworks and some branches

---
+++ sso-server/src/main/java/com/bitum10/sso/controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.bitum10.sso.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User:  lpm【百墨】 shouli1990@gmail.com
+ * Date: 14-6-4
+ * Time: 上午10:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomeController {
+}
